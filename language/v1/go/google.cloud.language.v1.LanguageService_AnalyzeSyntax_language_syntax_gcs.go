f39e8e581f42d5f7e958ca151742588754183322
Michael Darakananda <pongad@google.com>
2018-10-11 15:44:57 -0700

    add go samples

---
+++ language/v1/go/google.cloud.language.v1.LanguageService_AnalyzeSyntax_language_syntax_gcs.go
@@ -0,0 +1,65 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// AUTO-GENERATED CODE. DO NOT EDIT.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	language "cloud.google.com/go/language/apiv1"
+	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
+)
+
+// [START language_syntax_gcs]
+
+func sampleAnalyzeSyntax(arg0 string) error {
+	ctx := context.Background()
+	c, err := language.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// arg0 := "Path to text file, e.g. gs://my-bucket/textfile"
+	req := &languagepb.AnalyzeSyntaxRequest{
+		Document: &languagepb.Document{
+			Type: languagepb.Document_PLAIN_TEXT,
+			Source: &languagepb.Document_GcsContentUri{
+				GcsContentUri: arg0,
+			},
+		},
+	}
+	resp, err := c.AnalyzeSyntax(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	tokens := resp.GetTokens()
+	for _, token := range tokens {
+		fmt.Printf("Part of speech: %v\n", token.GetPartOfSpeech().GetTag())
+		fmt.Printf("Text:\n", token.GetText())
+	}
+	return nil
+}
+
+// [END language_syntax_gcs]
+
+func main() {
+	arg0 := flag.String("arg0", "Path to text file, e.g. gs://my-bucket/textfile", "")
+	flag.Parse()
+	sampleAnalyzeSyntax(*arg0)
+}
